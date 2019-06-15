748630912399e658dfba700ab6fad4adae79affe
Asya Troyan <asya99@yandex.ru>
2019-06-15 11:07:34 +0500

    plug-in EMDK

--- app/src/main/java/ru/zferma/zebrascanner/MainActivity.java
+++ app/src/main/java/ru/zferma/zebrascanner/MainActivity.java
@@ -1,31 +1,20 @@
 package ru.zferma.zebrascanner;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.symbol.emdk.EMDKManager;
+import com.symbol.emdk.EMDKManager.EMDKListener;
+
+public class MainActivity extends Activity implements EMDKListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +37,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onOpened(EMDKManager emdkManager) {
+        
+    }
+
+    @Override
+    public void onClosed() {
+
     }
 }
