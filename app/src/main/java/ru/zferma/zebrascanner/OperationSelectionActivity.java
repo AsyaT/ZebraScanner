c10e7de0b504508bd2c7ed20a8208906c88de1fb
Asya Troyan <asya99@yandex.ru>
2019-08-06 23:59:05 +0500

    activities - make switcher to activities

--- app/src/main/java/ru/zferma/zebrascanner/OperationSelectionActivity.java
+++ app/src/main/java/ru/zferma/zebrascanner/OperationSelectionActivity.java
@@ -1,8 +1,13 @@
 package ru.zferma.zebrascanner;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,6 +19,9 @@ public class OperationSelectionActivity extends AppCompatActivity {
     ListView operationsListView;
     ArrayList<String> listItem;
     Map<Integer,AccountingAreaModel> AccountingAreaIncomeData;
+    Map<String,String> OperationsList;
+
+    Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +29,40 @@ public class OperationSelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_operation_selection);
 
         operationsListView = (ListView)findViewById(R.id.OperationListView);
+        okButton = (Button) findViewById(R.id.OKButton);
 
         AccountingAreaIncomeData = new HashMap<Integer,AccountingAreaModel>();
         AccountingAreaIncomeData.put(1, new AccountingAreaModel("Инвентаризация", "gr-bbg-erb-r","Инвентаризация склад 12-1","gv8df-bdfb8dfb-dfb8"));
         AccountingAreaIncomeData.put(2, new AccountingAreaModel("Перемещение", "6r-ne-6jme2-0lbd1r","Межцеховой учет УПК",",qpr7-bvnt0-3y5n-dit"));
 
         listItem = new ArrayList<>();
+        OperationsList = new HashMap<String,String>();
         for (Map.Entry<Integer,AccountingAreaModel> accountingArea:AccountingAreaIncomeData.entrySet())
         {
             listItem.add( accountingArea.getValue().OperationType );
+            OperationsList.put(accountingArea.getValue().GuideOperationType, accountingArea.getValue().OperationType);
         }
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listItem);
 
         operationsListView.setAdapter(adapter);
 
+        operationsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+              @Override
+              public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                  view.setBackgroundColor(Color.YELLOW);
+
+
+              }
+          });
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAreaSelectoinIntent = new Intent(getBaseContext(),MainActivity.class);
+                startActivity( goToAreaSelectoinIntent );
+            }
+        });
     }
 
 }
