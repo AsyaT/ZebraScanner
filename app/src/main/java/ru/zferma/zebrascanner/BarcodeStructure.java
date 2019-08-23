c627b4b4c40321d76c2232c9318b1c72e4cadbc6
Asya Troyan <asya99@yandex.ru>
2019-08-23 23:25:29 +0500

    databar - refactoring

--- app/src/main/java/ru/zferma/zebrascanner/BarcodeStructure.java
+++ app/src/main/java/ru/zferma/zebrascanner/BarcodeStructure.java
@@ -16,14 +16,21 @@ public class BarcodeStructure {
 
     public BarcodeStructure(String fullBarcode)
     {
+        // Type of barcode id Weight Barcode
         if (fullBarcode.startsWith("2")) {
             Weight = fullBarcode.substring(7, 12);
             UniqueIdentifier = fullBarcode.substring(0, 7);
         }
-
-        if(fullBarcode.startsWith("01")) {
+        // Type of barcode is databar
+        else if(fullBarcode.startsWith("01")) {
             Weight = fullBarcode.substring(20,26);
             UniqueIdentifier = fullBarcode.substring(2,16);
+        }
+        // EAN 13
+        else
+        {
+            UniqueIdentifier = fullBarcode;
+            Weight = null;
         }
     }
 }
