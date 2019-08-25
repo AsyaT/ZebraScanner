d15bc19d1aae11234ed93f9302b764875509768c
Asya Troyan <asya99@yandex.ru>
2019-08-25 15:44:31 +0500

    databar - refactoring weight parse

--- app/src/main/java/ru/zferma/zebrascanner/BarcodeStructure.java
+++ app/src/main/java/ru/zferma/zebrascanner/BarcodeStructure.java
@@ -26,21 +26,22 @@ public class BarcodeStructure {
     {
         LabelType = labelType;
 
-        // Type of barcode id Weight Barcode
-        if (fullBarcode.startsWith("2")) {
-            Weight = fullBarcode.substring(7, 12);
-            UniqueIdentifier = fullBarcode.substring(0, 7);
-        }
-        // Type of barcode is databar
-        else if(fullBarcode.startsWith("01")) {
+        if(LabelType == ScanDataCollection.LabelType.GS1_DATABAR_EXP)
+        {
             Weight = fullBarcode.substring(20,26);
             UniqueIdentifier = fullBarcode.substring(2,16);
         }
-        // EAN 13
-        else
+        else if( LabelType == ScanDataCollection.LabelType.EAN13)
         {
-            UniqueIdentifier = fullBarcode;
-            Weight = null;
+            if (fullBarcode.startsWith("2")) {
+                Weight = fullBarcode.substring(7, 12);
+                UniqueIdentifier = fullBarcode.substring(0, 7);
+            }
+            else
+            {
+                UniqueIdentifier = fullBarcode;
+                Weight = null;
+            }
         }
     }
 }
