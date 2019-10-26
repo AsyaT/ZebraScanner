0a6bf535b1990b22fbd22e1403f92f493d452020
Asya Troyan <asya99@yandex.ru>
2019-10-27 00:17:49 +0500

    activities: select accounting area

--- app/src/main/java/ru/zferma/zebrascanner/AccountAreaSelectionActivity.java
+++ app/src/main/java/ru/zferma/zebrascanner/AccountAreaSelectionActivity.java
@@ -1,8 +1,13 @@
 package ru.zferma.zebrascanner;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,6 +17,10 @@ public class AccountAreaSelectionActivity extends AppCompatActivity {
 
     ListView accountAreasListView;
     ArrayList<String> listItem;
+    String SelectedAccountingArea="";
+
+    Button okButton;
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,8 @@ public class AccountAreaSelectionActivity extends AppCompatActivity {
         TextView operationTypeTextView = (TextView) findViewById(R.id.OperationTypeTextView);
         operationTypeTextView.setText(operationName);
 
+        okButton = (Button) findViewById(R.id.OKButtonAA);
+        cancelButton = (Button) findViewById(R.id.CancelButtonAA);
 
         OperationTypes AccountingAreaIncomeData = new OperationTypes();
         listItem = AccountingAreaIncomeData.GetAccountingAreas(operationName);
@@ -31,6 +42,51 @@ public class AccountAreaSelectionActivity extends AppCompatActivity {
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listItem); // WHAT Is IT "simple_list_item_1" ???
 
         accountAreasListView.setAdapter(adapter);
+
+        accountAreasListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String tap = ((TextView)view).getText().toString();
+
+                if(SelectedAccountingArea.isEmpty() || SelectedAccountingArea != tap)
+                {
+                    for (int i = 0; i < accountAreasListView.getChildCount(); i++) {
+                        View listItem = accountAreasListView.getChildAt(i);
+                        listItem.setBackgroundColor(Color.WHITE);
+                    }
+
+                    view.setBackgroundColor(Color.YELLOW);
+                    SelectedAccountingArea = tap;
+                }
+                else{
+                    view.setBackgroundColor(Color.WHITE);
+                    SelectedAccountingArea = "";
+                }
+            }
+        });
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            Intent goToMainActivityIntent = new Intent(getBaseContext(), MainActivity.class);
+            goToMainActivityIntent.putExtra("accounting_area_name", SelectedAccountingArea);
+            startActivity(goToMainActivityIntent);
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (int i = 0; i < accountAreasListView.getChildCount(); i++) {
+                    View listItem = accountAreasListView.getChildAt(i);
+                    listItem.setBackgroundColor(Color.WHITE);
+                }
+                SelectedAccountingArea = "";
+            }
+        });
+
 
     }
 }
