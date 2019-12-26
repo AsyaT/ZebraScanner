13f844cfe1ddc082c57809455f3f7e787d6fa4e0
Asya Troyan <asya99@yandex.ru>
2019-12-26 21:40:14 +0500

    OrderParsing - fix progress table

--- app/src/main/java/ru/zferma/zebrascanner/ProgressOrderFragment.java
+++ app/src/main/java/ru/zferma/zebrascanner/ProgressOrderFragment.java
@@ -2,6 +2,7 @@ package ru.zferma.zebrascanner;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,6 +48,7 @@ public class ProgressOrderFragment extends Fragment {
 
                 TextView txtView = new TextView(getActivity());
                 txtView.setLayoutParams(new TableRow.LayoutParams(120, TableRow.LayoutParams.WRAP_CONTENT));
+                txtView.setTextSize(10);
                 txtView.setText(
                         appState.productStructureModel.FindProductByGuid(product.GetProductGuid()) +
                                 "\n" +
@@ -58,17 +60,20 @@ public class ProgressOrderFragment extends Fragment {
 
                 TextView txtOrderd = new TextView(getActivity());
                 txtOrderd.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+                txtOrderd.setGravity(Gravity.CENTER);
                 txtOrderd.setText(product.OrderedInKilos().toString());
                 txtOrderd.setBackgroundResource(R.drawable.textviewborder);
 
                 TextView txtExecuted = new TextView(getActivity());
                 txtExecuted.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+                txtExecuted.setGravity(Gravity.CENTER);
                 Double finalSum = product.DoneInKilos() + doneKilos;
                 txtExecuted.setText(finalSum.toString());
                 txtExecuted.setBackgroundResource(R.drawable.textviewborder);
 
                 TextView txtLeft = new TextView(getActivity());
                 txtLeft.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+                txtLeft.setGravity(Gravity.CENTER);
                 Double finalDiff = product.LeftInKilos() - doneKilos;
                 txtLeft.setText(finalDiff.toString());
                 txtLeft.setBackgroundResource(R.drawable.textviewborder);
@@ -82,17 +87,20 @@ public class ProgressOrderFragment extends Fragment {
 
                 TextView txtOrderedPieces = new TextView(getActivity());
                 txtOrderedPieces.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+                txtOrderedPieces.setGravity(Gravity.CENTER);
                 txtOrderedPieces.setText(product.OrderedInItems().toString());
                 txtOrderedPieces.setBackgroundResource(R.drawable.textviewborder);
 
                 TextView txtExecutedPieces = new TextView(getActivity());
                 txtExecutedPieces.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+                txtExecutedPieces.setGravity(Gravity.CENTER);
                 Integer finalSumItems = product.DoneInItems() + doneItems;
                 txtExecutedPieces.setText(finalSumItems.toString());
                 txtExecutedPieces.setBackgroundResource(R.drawable.textviewborder);
 
                 TextView txtLeftPieces = new TextView(getActivity());
                 txtLeftPieces.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+                txtLeftPieces.setGravity(Gravity.CENTER);
                 Integer finalDiffItems = product.LeftInItems() - doneItems;
                 txtLeftPieces.setText(finalDiffItems.toString());
                 txtLeftPieces.setBackgroundResource(R.drawable.textviewborder);
