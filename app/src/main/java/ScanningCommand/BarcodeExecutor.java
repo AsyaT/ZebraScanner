33b4efe162d23b905840b4c841ae5bbd48dae0bd
Asya Troyan <asya99@yandex.ru>
2020-01-18 15:27:17 +0500

    PackeageList - set condition for PackageList scanning

--- app/src/main/java/ScanningCommand/BarcodeExecutor.java
+++ app/src/main/java/ScanningCommand/BarcodeExecutor.java
@@ -8,6 +8,13 @@ import businesslogic.ScannerState;
 
 public class BarcodeExecutor {
     public void Execute(ScannerState state, ScanDataCollection.ScanData data, Activity activity) throws InstantiationException, IllegalAccessException {
+
+        //TODO : set proper LabelType
+        if(state == ScannerState.PRODUCT && data.getLabelType() == ScanDataCollection.LabelType.GS1_DATABAR) // TODO: also might be scanned GUID of PAckegeList
+        {
+            state = ScannerState.PACKAGELIST;
+        }
+
         Class classCommand = state.GetClass();
         Command command = (Command) classCommand.newInstance();
         command.Action(activity);
