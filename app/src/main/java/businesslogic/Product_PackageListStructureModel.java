439d0dfe6b077f46872244c0ac41420505545e7e
Asya Troyan <asya99@yandex.ru>
2020-01-28 16:21:58 +0500

    PackageList - create constructor

--- app/src/main/java/businesslogic/Product_PackageListStructureModel.java
+++ app/src/main/java/businesslogic/Product_PackageListStructureModel.java
@@ -1,9 +1,28 @@
 package businesslogic;
 
+import java.util.Date;
+
 public class Product_PackageListStructureModel   extends ProductModel{
 
     protected String SSCCCode; //TODO : Maybe it related to Package List?
-    protected Integer Items; //TODO: send to result models
+    protected Integer Items; // Now is using in PackageListCommand. TODO: move to logic?
+
+    public Product_PackageListStructureModel(String productGuid,
+                                             String characteristicGuid,
+                                             Double weight,
+                                             Date production,
+                                             Date expired,
+                                             String manufacturerGuid,
+                                             Integer items)
+    {
+        this.ProductGUID = productGuid;
+        this.CharacteristicGUID = characteristicGuid;
+        this.Weight = weight;
+        this.ExpirationDate = production;
+        this.ProductionDate = expired;
+        this.ManufacturerGUID = manufacturerGuid;
+        this.Items = items;
+    }
 
     public Integer GetItems()
     {
